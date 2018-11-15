5e512877d9c8a232073941f4c9cfd3ea8a1f6bf3
Jarrod Johnson <jjohnson@new-innov.com>
2018-11-15 13:47:37 -0500

    Add c# examples

---
+++ Examples/LargeInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Examples
+{
+    public interface LargeInterface
+    {
+        void Method1();
+        void Method2();
+        void Method3();
+        void Method4();
+        void Method5();
+        void Method6();
+        void Method7();
+        void Method8();
+        void Method9();
+    }
+}
